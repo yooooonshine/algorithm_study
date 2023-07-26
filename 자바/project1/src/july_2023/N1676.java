bbf56b4d5035d27dc33e9a21f406b8b09f221225
yooooonshine <hl3470@naver.com>
2023-07-26 14:32:58 +0900

    Feat: 17087 숨바꼭질6 성공(자바), 2004 조합 0의 개수 실패, 다시 풀어볼 필요없다.

--- 자바/project1/src/july_2023/N1676.java
+++ 자바/project1/src/july_2023/N1676.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class N2004 { // 뒤의 0의 개수는 나온 5의 개수와 동일하다.
+public class N1676 { // 뒤의 0의 개수는 나온 5의 개수와 동일하다.
     public void func() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int Number5Counter = 0;
