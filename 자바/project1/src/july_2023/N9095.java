0e7b8bc92441a1749b7ac06f5475b2116246086f
yooooonshine <hl3470@naver.com>
2023-08-04 13:43:29 +0900

    Feat: 9095 1,2,3 더하기 성공(자바)

---
+++ 자바/project1/src/july_2023/N9095.java
@@ -0,0 +1,25 @@
+package july_2023;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class N9095 {
+    public int[] intList = new int[11];
+    public void func() throws IOException {
+        intList[0] = 0;
+        intList[1] = 1;
+        intList[2] = 2;
+        intList[3] = 4;
+        for(int j = 4; j <= 10; j++){
+            intList[j] = intList[j - 1] + intList[j - 2] + intList[j - 3];
+        }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for(int i = 0; i < n; i++){
+            int x = Integer.parseInt(br.readLine());
+            System.out.println(intList[x]);
+        }
+    }
+
+}
