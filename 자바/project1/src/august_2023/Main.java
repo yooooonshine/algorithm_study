ab048fcc0a491f598c56162dc05361915e240de0
yooooonshine <hl3470@naver.com>
2023-08-12 13:11:25 +0900

    Feat: 14002 가장 긴 증가하는 부분 수열4 성공(자바)

--- 자바/project1/src/august_2023/Main.java
+++ 자바/project1/src/august_2023/Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        N11053 my_class = new N11053();
+        N14002 my_class = new N14002();
         my_class.func();
     }
 }
