2830c4822329effb63111d11bd729402f34d0816
yooooonshine <hl3470@naver.com>
2023-09-02 14:52:23 +0900

    Feat: 1476 날짜계산 성공(자바)

--- 자바/project1/src/august_2023/Main.java
+++ 자바/project1/src/august_2023/Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        N1107 my_class = new N1107();
+        N1476 my_class = new N1476();
         my_class.func();
     }
 }
