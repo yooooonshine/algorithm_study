fe3a6b6a6c3d6bd5fc22f1dcf5329b398056ab7d
hl3470 <hl3470@naver.com>
2024-07-13 00:05:59 +0900

    refactor: N20291 다시 풀이

--- 자바/project1/src/year2024/month7_2024/N20291/Main.java
+++ 자바/project1/src/year2024/month7_2024/N20291/Main.java
@@ -8,44 +8,25 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int N = Integer.parseInt(br.readLine()); //파일 개수
-		List<String> extensions = new ArrayList<>();
+		Map<String, Integer> extensions = new HashMap<>();
+		List<String> extensionStrings = new ArrayList<>();
 
 		for (int i = 0; i < N; i++) {
 			String[] split = br.readLine().split("\\.");
 			String extension = split[1];
 
-			extensions.add(extension);
-		}
-
-		Collections.sort(extensions);
-
-		if (N == 1) {
-			System.out.println(extensions.get(0) + " 1");
-			return;
-		}
-
-		int count = 1;
-		for (int i = 0; i < N - 1; i++) {
-			String now = extensions.get(i);
-			String next = extensions.get(i + 1);
-
-			if (Objects.equals(now, next)) { //다음이랑 같을 경우
-				count++;
-
-				if (i == N - 2) { //다를 경우 마지막
-					System.out.println(now + " " + count);
-				}
+			if (extensions.get(extension) == null) {
+				extensionStrings.add(extension);
+				extensions.put(extension, 1);
 			} else {
-				System.out.println(now + " " + count); //다를 경우
-				count = 1;
-
-				if (i == N - 2) { //다를 경우 마지막
-					System.out.println(next + " " + count);
-				}
+				extensions.put(extension, extensions.get(extension) + 1);
 			}
-
-
 		}
+
+		Collections.sort(extensionStrings);
+
+		extensionStrings.stream()
+			.forEach(s -> System.out.println(s + " " +extensions.get(s)));
 	}
 }
 
