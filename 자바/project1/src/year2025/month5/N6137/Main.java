1b7bf019907091b4138f4d1596abdb608355a2bf
yooooonshine <hl3470@naver.com>
2025-05-30 10:58:54 +0900

    feat: N6137 성공(자바)

--- 자바/project1/src/year2025/month5/N6137/Main.java
+++ 자바/project1/src/year2025/month5/N6137/Main.java
@@ -19,12 +19,35 @@ public class Main {
 		}
 
 		result = new char[N + 1]; //최종 결과 배열
-		tmp = new char[N + 1]; // 임시 배열
-		for (int i = 1; i <= N; i++) {
-			result[i] = 'Z'; // 초기값을 가장 큰 문자로 설정
-		}
 
-		recursion(1, N, 1); // 재귀 호출 시작 (1부터 N까지)
+		int index = 1;
+		int s = 1;
+		int e = N;
+		while (s <= e) {
+
+			boolean left = false;
+
+			int sIndex = s;
+			int eIndex = e;
+
+			while (sIndex <= eIndex) {
+				if (S[sIndex] < S[eIndex]) {
+					left = true;
+					break;
+				} else if (S[sIndex] > S[eIndex]) {
+					left = false;
+					break;
+				}
+				sIndex++;
+				eIndex--;
+			}
+
+			if (left) {
+				result[index++] = S[s++];
+			} else {
+				result[index++] = S[e--];
+			}
+		}
 
 		// 80글자마다 새줄문자 출력!
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -38,51 +61,6 @@ public class Main {
 	}
 
 	public static char[] result;
-	public static char[] tmp;
-
-	public static void recursion(int s, int e, int index) {
-		if (index > N) {
-			if (isFast(tmp, result)) {
-				replace(result, tmp);
-			}
-
-			return;
-		}
-
-		if (S[s] - S[e] > 0) {
-			tmp[index] = S[e];
-			recursion(s, e - 1, index + 1);
-
-		} else if (S[s] - S[e] == 0) {
-			tmp[index] = S[s];
-			recursion(s + 1, e, index + 1);
-
-			tmp[index] = S[e];
-			recursion(s, e - 1, index + 1);
-		} else {
-			tmp[index] = S[s];
-			recursion(s + 1, e, index + 1);
-		}
-	}
-	// ABCBCD
-	// ABCCBD
-	public static boolean isFast(char[] a, char[] b) {
-		for (int i = 1; i < a.length; i++) {
-			if (a[i] > b[i]) {
-				return false;
-			}
-			if (a[i] < b[i]) {
-				return true;
-			}
-		}
-		return true;
-	}
-
-	public static void replace(char[] a, char[] b) {
-		for (int i = 1; i < a.length; i++) {
-			a[i] = b[i];
-		}
-	}
 }
 
 // N개의 문자로 이뤄진 문자열 S
